3077c0a9e94fcf325d2deccb596b1ee6aed4731f
Francis Hilaire <Prometee@users.noreply.github.com>
2022-11-30 19:57:21 +0100

    Remove deprecated for Symfony 6, a workaround have to be found first

---
+++ Tests/PrometeeVIESClientBundleTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Prometee\VIESClientBundle;
+
+use Prometee\VIESClientBundle\VIESClientBundle;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PrometeeVIESClientBundleTest extends TestCase
+{
+    public function test__construct(): void
+    {
+        $bundle = new VIESClientBundle();
+
+        $this->assertInstanceOf(Bundle::class, $bundle);
+    }
+
+    public function testGetPath(): void
+    {
+        $bundle = new VIESClientBundle();
+        $this->assertEquals(dirname(__DIR__), $bundle->getPath());
+    }
+}
